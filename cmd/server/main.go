3715dc02feaa65b85db5f8237f49fa25f4228571
otvet8 <arsabramoff@gmail.com>
2022-12-28 18:16:10 +0300

    added server main.go file

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,27 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/AbramovArseniy/YandexRuntimeMetrics/internal/server"
+)
+
+const (
+	Server = "127.0.0.1"
+	Port   = "8080"
+)
+
+func StartServer() {
+	srv := &http.Server{
+		Addr: Server + ":" + Port,
+	}
+	http.HandleFunc("/update/", server.PostMetricHandler)
+	log.Println("Server started")
+	log.Fatal(srv.ListenAndServe())
+
+}
+
+func main() {
+	StartServer()
+}
